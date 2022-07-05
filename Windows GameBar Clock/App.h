9a71063a0b78fd02ae0b573741003a6e9d880298
nelson-tbone <tbone.nelson@gmail.com>
2022-07-05 01:10:57 -0300

    Add project files.

---
+++ Windows GameBar Clock/App.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::Windows_GameBar_Clock::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+        void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+    };
+}
