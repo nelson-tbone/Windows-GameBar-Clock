4d6ad9f6115fc3a04edf4d43860a1c595f2a5fa1
nelson-tbone <tbone.nelson@gmail.com>
2022-07-05 22:25:15 -0300

    Master com o dialog box funcionando.

--- Windows GameBar Clock/MainPage.cpp
+++ Windows GameBar Clock/MainPage.cpp
@@ -1,6 +1,7 @@
 ﻿#include "pch.h"
 #include "MainPage.h"
 #include "MainPage.g.cpp"
+#include "winrt/Windows.UI.Popups.h"
 
 using namespace winrt;
 using namespace Windows::UI::Xaml;
@@ -25,5 +26,10 @@ namespace winrt::Windows_GameBar_Clock::implementation
     void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
     {
         myButton().Content(box_value(L"Clicked"));
+        winrt::hstring Title = L"Hello";
+        winrt::hstring Content = L"Some cool content!";
+        winrt::Windows::UI::Popups::MessageDialog dialog(Content, Title);
+        dialog.ShowAsync();
     }
+   
 }
